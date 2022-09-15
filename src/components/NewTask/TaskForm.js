af1bffd619837808f589ceb2dff4664c7dc0843e
Samaresh Das <samareshmail679@gmail.com>
2022-09-15 16:08:44 +0530

    added clear on submit

--- src/components/NewTask/TaskForm.js
+++ src/components/NewTask/TaskForm.js
@@ -1,15 +1,24 @@
+import { useState } from "react";
 import { useRef } from "react";
 
 const TaskForm = (props) => {
+  const [isValid, setIsValid] = useState(true);
   const taskDataRef = useRef();
 
   const submitHandler = (event) => {
     event.preventDefault();
 
     const taskData = taskDataRef.current.value;
+    if (taskData.trim() === "") {
+      setIsValid(false);
+      return;
+    }
+
     if (taskData.trim().length > 0) {
       props.onEnterTask(taskData);
     }
+
+    taskDataRef.current.value = "";
   };
 
   return (
@@ -19,6 +28,7 @@ const TaskForm = (props) => {
         className="form-control form-control-lg"
         type="text"
       />
+      {!isValid && <p className="text-danger">Input cannot be empty</p>}
       <button className="btn btn-success mt-3">
         {props.loading ? "Adding....." : "Add Task"}
       </button>
